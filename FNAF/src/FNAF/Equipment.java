95edd766be0e458c2ad1a01447521b532e2d86f9
Ezra <Ezra@LAPTOP-9PTD8HUA>
2023-12-27 22:48:05 +0800

    Merge origin/master

---
+++ FNAF/src/FNAF/Equipment.java
@@ -0,0 +1,18 @@
+package FNAF;
+
+import .*;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ezra
+ */
+public class Equipment {
+    public Equipment[] createEquipment(){
+        
+    }
+}
